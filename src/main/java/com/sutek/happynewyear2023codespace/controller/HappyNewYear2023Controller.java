029a8e1fc9eeb4d658b8bc5096464ada1b912a29
Brian Su <briansu2004@hotmail.com>
2022-12-31 16:57:48 -0500

    $

--- src/main/java/com/sutek/happynewyear2023codespace/controller/HappyNewYear2023Controller.java
+++ src/main/java/com/sutek/happynewyear2023codespace/controller/HappyNewYear2023Controller.java
@@ -5,8 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/happy/v1")
 public class HappyNewYear2023Controller {
-	@GetMapping(value="/")
+	@GetMapping(value="happynewyear2023")
 	public String happyNewYear2023() {
 		return "Happy New Year 2023!";
 	}
